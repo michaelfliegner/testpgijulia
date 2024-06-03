7e95a603fbd4d1ef4037d9f99a35b28af260f714
Michael Fliegner <12422482+michaelfliegner@users.noreply.github.com>
2024-06-03 20:29:12 +0000

    startup.jl added

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,3 +2,9 @@
 set -e
 
 julia -e "using Pkg; Pkg.add(\"IJulia\")"
+
+cat << EOF > ~/.julia/config/startup.jl 
+ENV["GENIE_DEV"] = "dev"
+ENV["SEARCHLIGHT_USERNAME"] = "postgres"
+ENV["SEARCHLIGHT_PASSWORD"] = "postgres"
+EOF
