2f7c8ca79328b802556e0d899b45f6cdc12ac025
Michael Fliegner <12422482+michaelfliegner@users.noreply.github.com>
2024-06-03 21:17:27 +0000

    SEARCHLIGHT connects

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,6 +3,7 @@ set -e
 
 julia -e "using Pkg; Pkg.add(\"IJulia\")"
 
+mkdir ~/.julia/config
 cat << EOF > ~/.julia/config/startup.jl 
 ENV["GENIE_DEV"] = "dev"
 ENV["SEARCHLIGHT_USERNAME"] = "postgres"
